d8169299cd5efaf0be2cb70e001c3cea99b3b0ef
justinecua <jcua.dev@gmail.com>
2026-04-16 19:25:28 +0800

    development setup

---
+++ frontend/App.tsx
@@ -0,0 +1,8 @@
+import RegisterScreen from './src/screens/RegisterScreen';
+import SplashScreen from './src/screens/SplashScreen';
+
+function App() {
+  return <RegisterScreen />;
+}
+
+export default App;
---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,64 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import styles from '../styles/RegisterScreenStyles';
+import colors from '../constants/colors';
+import goodFoodGreen from '../assets/images/goodFood_green.png';
+
+const RegisterScreen = () => {
+  return (
+    <View style={styles.background}>
+      <View style={styles.UpperLogoContainer}>
+        <Image style={styles.UpperLogo} source={goodFoodGreen} />
+      </View>
+      <View>
+        <View style={styles.topContainer}>
+          <Text style={styles.headings}>Create an account</Text>
+        </View>
+        <View style={styles.topContainer}>
+          <Text style={styles.subHeadings}>Join us and</Text>
+          <Text style={styles.subHeadings2}>find local foods to eat!</Text>
+        </View>
+        <View style={styles.midContainer}>
+          <TextInput
+            placeholder="Username"
+            placeholderTextColor={colors.subtextInput}
+            style={styles.registerInput}
+          ></TextInput>
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor={colors.subtextInput}
+            style={styles.registerInput}
+          ></TextInput>
+          <TextInput
+            placeholder="Email Address"
+            placeholderTextColor={colors.subtextInput}
+            style={styles.registerInput}
+          ></TextInput>
+          <TextInput
+            placeholder="Role Selection"
+            placeholderTextColor={colors.subtextInput}
+            style={styles.registerInput}
+          ></TextInput>
+        </View>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <View style={styles.bottomContent}>
+          <View style={styles.bottomSubContainer}>
+            <Text style={styles.bottomsubHeading}>
+              Already have an account?
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.bottomsubHeadingLogin}>Login</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.registerButton}>
+          <Text style={styles.registerButtonText}>Continue</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default RegisterScreen;
