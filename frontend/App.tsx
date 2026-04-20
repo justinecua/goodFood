1fc5e2db073dbe45751be1a981ab9b4b0202a516
luke <luke062206@gmail.com>
2026-04-20 19:53:51 +0800

    unfinished restaurant home screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,6 @@
 import RegisterScreen from './src/screens/RegisterScreen';
 import SplashScreen from './src/screens/SplashScreen';
+import RestaurantHomeScreen from './src/screens/RestaurantHomeScreen';
 
 function App() {
   return <RegisterScreen />;
